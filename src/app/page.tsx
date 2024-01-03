1990c8e29bae74f913d570cd31247fc1159a5ba6
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-03 20:40:36 +0900

    add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <>home</>
+    <>
+      <section className='my-12'>
+        <Image
+          className='rounded-full border-2 border-slate-200 mb-8'
+          src='/images/profile-image.png'
+          alt='Profile image'
+          width={108}
+          height={108}
+        />
+        <p>Hello, I am a software developer based in Japan.</p>
+      </section>
+    </>
   );
 }
