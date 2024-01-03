b2f5f02e05394e857ef38697c66c6c4886dcef1c
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-03 21:59:45 +0900

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Courier_Prime } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import Footer from "@/components/footer";
 
 const courierPrime = Courier_Prime({
   weight: ["400", "700"],
@@ -25,6 +26,7 @@ export default function RootLayout({
         <div className='container max-w-3xl mx-auto px-4'>
           {children}
         </div>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="container max-w-3xl mx-auto px-4">
+        <div className="relative flex items-center justify-between h-16">
+          <p>&copy; 2024 Yuya Toyoizumi</p>
+        </div>
+      </div>
+    </>
+  )
+}
