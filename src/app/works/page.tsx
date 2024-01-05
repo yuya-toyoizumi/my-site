6cfb7788659c7cf5f791dab40f9dd8c5dd20409d
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-05 18:40:23 +0900

    update works style

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -6,36 +6,36 @@ export default function Works() {
       <div className='flex flex-col gap-4'>
         <h2 className='text-xl font-bold'>Works</h2>
         <div>
-          <div className='flex flex-wrap justify-between'>
+          <div className='flex flex-wrap justify-center sm:justify-around gap-8'>
             <div className='flex flex-col gap-2 items-start'>
               <h3 className='font-bold'>sake no kiroku (酒の記録)</h3>
-              <div className='w-80 sm:w-80'>
+              <div className='w-80 sm:w-80 max-w-full'>
                 <Image
                   className='border border-slate-200'
                   src='/images/sake-no-kiroku.png'
                   alt='sake no kiroku image'
                   layout='responsive'
-                  width={100}
-                  height={100}
+                  width={16}
+                  height={9}
                 />
               </div>
-              <p className='w-80'>
+              <p className='w-80 max-w-full'>
                 This is a drinking diary app that allows you to easily record your alcohol consumption and visualize your drinking and alcohol intake.
               </p>
             </div>
             <div className='flex flex-col gap-2 items-start'>
               <h3 className='font-bold'>rhythmo</h3>
-              <div className='w-80 sm:w-80'>
+              <div className='w-80 sm:w-80 max-w-full'>
                 <Image
                   className='border border-slate-200'
                   src='/images/rhythmo.png'
                   alt='sake no kiroku image'
                   layout='responsive'
-                  width={100}
-                  height={100}
+                  width={16}
+                  height={9}
                 />
               </div>
-              <p className='w-80'>
+              <p className='w-80 max-w-full'>
                 This is a habit-forming app with personalized tracking and a heat map to visualize progress, perfect for various goals like exercise and reading.
               </p>
             </div>
