682c3d52c088eaf44b9bfbd0466a4a5721aa10c4
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-05 18:42:34 +0900

    update works style

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -9,7 +9,7 @@ export default function Works() {
           <div className='flex flex-wrap justify-center sm:justify-around gap-8'>
             <div className='flex flex-col gap-2 items-start'>
               <h3 className='font-bold'>sake no kiroku (酒の記録)</h3>
-              <div className='w-80 sm:w-80 max-w-full'>
+              <div className='w-80 sm:w-80'>
                 <Image
                   className='border border-slate-200'
                   src='/images/sake-no-kiroku.png'
@@ -19,13 +19,13 @@ export default function Works() {
                   height={9}
                 />
               </div>
-              <p className='w-80 max-w-full'>
+              <p className='w-80'>
                 This is a drinking diary app that allows you to easily record your alcohol consumption and visualize your drinking and alcohol intake.
               </p>
             </div>
             <div className='flex flex-col gap-2 items-start'>
               <h3 className='font-bold'>rhythmo</h3>
-              <div className='w-80 sm:w-80 max-w-full'>
+              <div className='w-80 sm:w-80'>
                 <Image
                   className='border border-slate-200'
                   src='/images/rhythmo.png'
@@ -35,7 +35,7 @@ export default function Works() {
                   height={9}
                 />
               </div>
-              <p className='w-80 max-w-full'>
+              <p className='w-80'>
                 This is a habit-forming app with personalized tracking and a heat map to visualize progress, perfect for various goals like exercise and reading.
               </p>
             </div>
