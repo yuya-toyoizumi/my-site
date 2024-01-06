f8dceaceabff83b397e728d78a26f65bf6fa3085
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-06 11:14:06 +0900

    add works layout

---
+++ src/app/works/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Works | Yuya Toyoizumi',
+  description: "Explore this curated showcase of my products, each demonstrating my skills and curiosity."
+};
+
+export default function WorksLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children
+}
