5c73eb59262b20b77482a9ccd1348207c8ac3b89
yuya-toyoizumi <gbr.eng123@gmail.com>
2024-01-08 11:47:13 +0900

    update ui

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -6,12 +6,14 @@ export default function Notes() {
     <>
       <div className="flex flex-col gap-4">
         <h2 className="text-xl font-bold">Notes</h2>
-        <div>
-          {allNotes.map((note) => (
-            <article key={note.slug}>
-              <Link href={`/notes/${note.slug}`}>{note.title}</Link>
-            </article>
-          ))}
+        <div className="flex flex-col gap-2">
+          {allNotes.length > 0
+            ? allNotes.map((note) => (
+                <article key={note.slug}>
+                  <Link href={`/notes/${note.slug}`}>{note.title}</Link>
+                </article>
+              ))
+            : "No notes yet."}
         </div>
       </div>
     </>
