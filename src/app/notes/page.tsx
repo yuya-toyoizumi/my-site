1f76f7b25fb3e20eda5604772142ff5c1145e137
Yuya Toyoizumi <gbr.eng123@gmail.com>
2026-04-07 07:09:40 +0900

    feat(notes): sort notes by date descending

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,13 +2,17 @@ import Link from "next/link";
 import { allNotes } from "contentlayer/generated";
 
 export default function Notes() {
+  const sortedNotes = [...allNotes].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
   return (
     <>
       <div className="flex flex-col gap-4">
         <h2 className="text-xl font-bold">Notes</h2>
         <div className="flex flex-col gap-2">
-          {allNotes.length > 0
-            ? allNotes.map((note) => (
+          {sortedNotes.length > 0
+            ? sortedNotes.map((note) => (
                 <article key={note.slug}>
                   <Link href={`/notes/${note.slug}`}>{note.title}</Link>
                 </article>
