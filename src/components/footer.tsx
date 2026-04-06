21fc78c6e15d7089f86c23a738a8e69041e4eb0e
Yuya Toyoizumi <gbr.eng123@gmail.com>
2026-04-07 07:08:55 +0900

    feat(footer): use dynamic year in copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <div className="container max-w-3xl mx-auto px-4">
         <div className="relative flex items-center justify-between h-16">
-          <p>&copy; 2024 Yuya Toyoizumi</p>
+          <p>&copy; {new Date().getFullYear()} Yuya Toyoizumi</p>
         </div>
       </div>
     </>
